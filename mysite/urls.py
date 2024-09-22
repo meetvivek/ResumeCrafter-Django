797a6379e77b2b1ace40895a0a7e454d8b229a7e
meetvivek <kumarvivek.july28@gmail.com>
2024-09-22 20:35:11 +0530

    Creater form for user data.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from resume import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('resume-form/', views.resume_form, name="resume_form"),
 ]
--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def resume_form(request):
+    return render(request, "resume/resume_form.html")
